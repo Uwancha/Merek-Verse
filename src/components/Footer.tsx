89f3f8999a86663289a698ab0a7357cc5afbd467
Uwancha <sito2alemu@gmail.com>
2023-10-27 23:31:28 +0300

    feat: Create Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+const Footer: React.FC = () => {
+
+    return (
+
+        <footer>
+            <section>
+                <a href="">Home</a>
+                <a href="">About Us</a>
+                <a href="">Contact</a>
+                <a href="">Blog</a>
+            </section>
+
+            <section className="links">
+                <a href=""><img src="" alt="link to YouTube" /></a>
+                <a href=""><img src="" alt="link to LinkedIn" /></a>
+                <a href=""><img src="" alt="link to TikTok" /></a>
+                <a href=""><img src="" alt="link to Twitter" /></a>
+                <a href=""><img src="" alt="link to GitHub" /></a>
+            </section>
+
+            
+        </footer>
+
+    )
+}
+
+export default Footer;
