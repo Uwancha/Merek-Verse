311d2ddeca32a346cd00d001db674789c01dba96
Uwancha <sito2alemu@gmail.com>
2023-10-28 20:34:56 +0300

    feat: Create Skills page Add skils with some high level categories

---
+++ src/pages/Skills.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+
+const Skills: React.FC = () => {
+
+    return (
+        <div>
+            <article>
+                
+                <h3>IT & Software Engineering</h3>
+                <ul>
+                    <li>Web Development</li>
+                    <li>Mobile App Development</li>
+                    <li>Software Engineering</li>
+                    <li>Cybersecurity</li>
+                    <li>Machine Learning</li>
+                    <li>Data Science</li>
+                    <li>Cloud Computing</li>
+                </ul>
+            </article>
+
+            <article>
+    
+                <h3>Design & Media</h3>
+                <ul>
+                    <li>Graphic Design</li>
+                    <li>UI/UX Design</li>
+                    <li>Video Editing</li>
+                    <li>Photography</li>
+                    <li>3D & Animation</li>
+                </ul>
+            </article>
+
+            <article>
+                <h3>Marketing & Sales</h3>
+                    <ul>
+                        <li>Digital Marketing</li>
+                        <li>Content Marketing</li>
+                        <li>Social Media Marketing</li>
+                        <li>Sales</li>
+                    </ul>
+            </article>
+
+            <article>
+                <h3>Lifestyle</h3>
+                <ul>
+                    <li>Health & Fitness</li>
+                    <li>Personal Development</li>
+                    <li>Languages</li>
+                    <li>Music</li>
+                </ul>
+            </article>
+            
+        </div>
+    )
+}
+
+export default Skills;
