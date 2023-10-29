ce57b945a23f3d84498c3fce4c60762de86a5303
Uwancha <sito2alemu@gmail.com>
2023-10-29 11:48:46 +0300

    style: Style and layout error page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/notfound.css"
 
 
 const NotFound: React.FC = () => {
   return (
-    <div>
+    <div className="notfound">
       <h2>opps! page not found </h2>
       <p>The page you are looking for does not exist.</p>
       <Link to="/">Back to Home page</Link>
