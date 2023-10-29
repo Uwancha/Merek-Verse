f664ef52795856ae93ee5ce9b125175f5be49f18
Uwancha <sito2alemu@gmail.com>
2023-10-29 19:40:47 +0300

    ui: Improve content flow of Skills page

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -9,84 +9,183 @@ const Skills: React.FC = () => {
             <article className="skill-category">
                 <h3>IT & Software Engineering</h3>
                 <section className="skills-section" >
-                    <Link to="/skills/web-dev" className="link" >
-                        Web Development
-                    </Link>
-                    <Link to="/skills/mobile-app" className="link" >
-                        Mobile App Development
-                    </Link>
-                    <Link to="/skills/software-engineering" className="link" >
-                        Software Engineering
-                    </Link>
-                    <Link to="/skills/cybersecurity" className="link" >
-                        Cybersecurity
-                    </Link>
-                    <Link to="/skills/machine-learning" className="link" >
-                        Machine Learning
-                    </Link>
-                    <Link to="/skills/data-science" className="link" >
-                        Data Science
-                    </Link>
-                    <Link to="/skills/cloud-coumputing" className="link" >
-                        Cloud Computing
-                    </Link>
+                    <section className="card" >
+                        <h4>Web Development</h4>
+                        <p>A brief introduction about the skill...</p>
+                        <Link to="/skills/web-dev" className="link" >
+                            View full details
+                        </Link>
+                    </section>
+                    <section className="card" >
+                        <h4>Mobile Development</h4>
+                        <p>A brief introduction about the skill...</p>
+
+                        <Link to="/skills/mobile-app" className="link" >
+                            View full detais
+                        </Link>
+                    </section>
+                    <section className="card" >
+                        <h4>Software Engineering</h4>
+                        <p>A brief introduction about the skill...</p>
+
+                        <Link to="/skills/software-engineering" className="link" >
+                            View full details
+                        </Link>
+                    </section>
+                    <section className="card" >
+                        <h4>Cyber Security</h4>
+                        <p>A brief introduction about the skill...</p>
+
+                        <Link to="/skills/cybersecurity" className="link" >
+                            View full details
+                        </Link>
+                    </section>
+                    <section className="card" >
+                        <h4>Machine Learning</h4>
+                        <p>A brief introduction about the skill...</p>
+
+                        <Link to="/skills/machine-learning" className="link" >
+                            View full details
+                        </Link>
+                    </section>
+                    <section className="card" >
+                        <h4>Data Science</h4>
+                        <p>A brief introduction about the skill...</p>
+
+                        <Link to="/skills/data-science" className="link" >
+                            View full details
+                        </Link>
+                    </section>
+                    <section className="card" >
+                        <h4>Cloud Computing</h4>
+                        <p>A brief introduction about the skill...</p>
+
+                        <Link to="/skills/cloud-coumputing" className="link" >
+                            View full details
+                        </Link>
+                    </section>
                 </section>
             </article>
 
             <article className="skill-category">
                 <h3>Design & Media</h3>
                 <section className="skills-section" >
-                    <Link to="/skills/graphic-design" className="link" >
-                        Graphic Design
-                    </Link>
-                    <Link to="/skills/ux-ui" className="link" >
-                        UI/UX Design
-                    </Link>
-                    <Link to="/skills/video-editing" className="link" >
-                        Video Editing
-                    </Link>
-                    <Link to="/skills/photography" className="link" >
-                        Photography
-                    </Link>
-                    <Link to="/skills/threeD"className="link" >
-                        3D & Animation
-                    </Link>
+                    <section className="card" >
+                        <h4>Graphic Design</h4>
+                        <p>A brief introduction about the skill...</p>
+
+                        <Link to="/skills/graphic-design" className="link" >
+                            View full details
+                        </Link>
+                    </section>
+                    <section className="card" >
+                        <h4>UX/UI Design</h4>
+                        <p>A brief introduction about the skill...</p>
+
+                        <Link to="/skills/ux-ui" className="link" >
+                            View full details
+                        </Link>
+                    </section>
+                    <section className="card" >
+                        <h4>Video Editing</h4>
+                        <p>A brief introduction about the skill...</p>
+
+                        <Link to="/skills/video-editing" className="link" >
+                            View full details
+                        </Link>
+                    </section>
+                    <section className="card" >
+                        <h4>Photoghraphy</h4>
+                        <p>A brief introduction about the skill...</p>
+
+                        <Link to="/skills/photography" className="link" >
+                            View full details
+                        </Link>
+                    </section>
+                    <section className="card" >
+                        <h4>3D & Animation</h4>
+                        <p>A brief introduction about the skill...</p>
+
+                        <Link to="/skills/threeD"className="link" >
+                            View full details
+                        </Link>
+                    </section>
                 </section>
             </article>
 
             <article className="skill-category">
                 <h3>Marketing & Sales</h3>
                 <section className="skills-section" >
-                    <Link to="/skills/digital-marketing" className="link" >
-                        Digital Marketing
-                    </Link>
-                    <Link to="/skills/content-marketing" className="link" >
-                        Content Marketing
-                    </Link>
-                    <Link to="/skills/social-media" className="link" >
-                        Social Media Marketing
-                    </Link>
-                    <Link to="/skills/sales" className="link" >
-                        Sales
-                    </Link>
+                    <section className="card" >
+                        <h4>Digital Marketing</h4>
+                        <p>A brief introduction about the skill...</p>
+
+                        <Link to="/skills/digital-marketing" className="link" >
+                            View full details
+                        </Link>
+                    </section>
+                    <section className="card" >
+                        <h4>Content Marketing</h4>
+                        <p>A brief introduction about the skill...</p>
+
+                        <Link to="/skills/content-marketing" className="link" >
+                            View full details
+                        </Link>
+                    </section>
+                    <section className="card" >
+                        <h4>Social Media Marketing</h4>
+                        <p>A brief introduction about the skill...</p>
+
+                        <Link to="/skills/social-media" className="link" >
+                            View full details
+                        </Link>
+                    </section>
+                    <section className="card" >
+                        <h4>Sales</h4>
+                        <p>A brief introduction about the skill...</p>
+
+                        <Link to="/skills/sales" className="link" >
+                            View full details
+                        </Link>
+                    </section>
                 </section>
             </article>
 
             <article className="skill-category">
                 <h3>Lifestyle</h3>
                 <section className="skills-section" >
-                    <Link to="/skills/health-health-fitness" className="link" >
-                        Health & Fitness
-                    </Link>
-                    <Link to="/skills/personal-development" className="link" >
-                        Personal Development
-                    </Link>
-                    <Link to="/skills/languages" className="link" >
-                        Languages
-                    </Link>
-                    <Link to="/skills/music" className="link" >
-                        Music
-                    </Link>
+                    <section className="card" >
+                        <h4>Health & Fitness</h4>
+                        <p>A brief introduction about the skill...</p>
+
+                        <Link to="/skills/health-health-fitness" className="link" >
+                            View full details
+                        </Link>
+                    </section>
+                    <section className="card" >
+                        <h4>Personal Development</h4>
+                        <p>A brief introduction about the skill...</p>
+
+                        <Link to="/skills/personal-development" className="link" >
+                            View full details
+                        </Link>
+                    </section>
+                    <section className="card" >
+                        <h4>Languages</h4>
+                        <p>A brief introduction about the skill...</p>
+
+                        <Link to="/skills/languages" className="link" >
+                            View full details
+                        </Link>
+                    </section>
+                    <section className="card" >
+                        <h4>Music</h4>
+                        <p>A brief introduction about the skill...</p>
+
+                        <Link to="/skills/music" className="link" >
+                            Music
+                        </Link>
+                    </section>
                 </section>
             </article>
 
