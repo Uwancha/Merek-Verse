5c0041020c0c462fcd8ea8ccc3894a5592b77e6b
Uwancha <sito2alemu@gmail.com>
2023-10-30 09:42:24 +0300

    feat: Create About page and add some placeholder contents

---
+++ src/pages/About.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+const About: React.FC = () => {
+
+    return (
+        <div>
+            <section>
+                <h2>About Us</h2>
+                <p>
+                    Merek is ldjld dfljdlkv lkvlkvj dflvjkdfkl dkljkvdlv
+                    jlkljlklkk kdflknljknj lkdfnlkd jdrgz,dvlze j;eivjkzdk flkjlf
+                </p>
+            </section>
+
+            <section>
+                <h4>What this site contains</h4>
+                <ul>
+                    <li>
+                        Curated quality self-study resources
+                    </li>
+                    <li>
+                        Best tips that saves your time and energy
+                    </li>
+
+                </ul>
+            </section>
+
+        </div>
+    )
+}
+
+export default About;
