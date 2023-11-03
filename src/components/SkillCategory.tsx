7223ebc07bdc3d45ac780582fd007e2eabfa1c8a
Uwancha <sito2alemu@gmail.com>
2023-11-03 21:56:29 +0300

    feat: Create SkillsCategory presentational component
    Extracted SkillsCategory component to render a single category
    Accepts title and cards props defined by Card interface
    Renders category header and passes cards to SkillsSection

---
+++ src/components/SkillCategory.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Card} from '../interfaces/Card';
+import SkillsSection from './SkillsSection';
+
+interface Props {
+    title: string;
+    cards: Card[];
+}
+
+const SkillsCategory = ({title, cards}: Props) => {
+
+    return (
+        <article className="skill-category">
+        <h3>{title}</h3>
+        <SkillsSection cards={cards}/>
+        </article>
+    );
+
+};
+
+export default SkillsCategory;
+
