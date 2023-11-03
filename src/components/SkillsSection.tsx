7b2963c4cfed13ba48cdd70b34bd07988a024e00
Uwancha <sito2alemu@gmail.com>
2023-11-03 21:57:15 +0300

    feat: Create SkillsSection presentational component
    Extracted SkillsSection to render skills cards list
    Accepts cards array defined by Card interface
    Maps over cards and renders SkillCard for each

---
+++ src/components/SkillsSection.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Card} from '../interfaces/Card';
+import SkillCard from './SkillCard';
+
+interface Props {
+    cards: Card[];
+}
+
+const SkillsSection = ({cards}: Props) => {
+
+    return (
+        <section className="skills-section">
+            {cards.map(card => (
+            <SkillCard key={card.id} {...card} />
+            ))}
+        </section>
+    );
+
+};
+
+export default SkillsSection;
