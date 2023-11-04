867699d85cbf3ba587717d97f16219d5d3a9fdf4
Uwancha <sito2alemu@gmail.com>
2023-11-04 21:43:23 +0300

    fix: Use updated prop name

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import {Card} from '../interfaces/Card';
 import {Link} from 'react-router-dom';
 
-const SkillCard = ({title, description, link}: Card) => {
+const SkillCard = ({name, description, link}: Card) => {
 
     return (
         <div className="card">
-            <h4>{title}</h4>
+            <h4>{name}</h4>
             <p>{description}</p>
             <Link to={link}>View Details</Link>
         </div>
