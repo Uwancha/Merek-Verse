573a6ade76bade4828a70c39406b374bec7a9d91
Uwancha <sito2alemu@gmail.com>
2023-11-05 14:45:35 +0300

    ui: Add styles to SkillCard component

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import {Card} from '../interfaces/Card';
 import {Link} from 'react-router-dom';
 
+import "../styles/skills-card.css";
+
 const SkillCard = ({name, description, link}: Card) => {
 
     return (
         <div className="card">
             <h4>{name}</h4>
             <p>{description}</p>
-            <Link to={link}>View Details</Link>
+            <Link to={link} className='link'>View Details</Link>
         </div>
     );
 };
