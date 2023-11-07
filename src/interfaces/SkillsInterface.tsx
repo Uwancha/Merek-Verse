a878042b86020a125cc27cea1f74183b2d32e075
Uwancha <sito2alemu@gmail.com>
2023-11-07 21:01:31 +0300

    feat: Define Content and Posts interface types
    This commit defines the Content and Posts interface types that will be used throughout the codebase to strongly type redux state, props, etc. related to skills and blog posts content.
    
    The Content interface defines the shape of category based content groups with an array of IT skills. The Posts interface defines the shape of individual blog post objects with title and content properties.
    
    Defining these interfaces upfront provides type safety and prevents bugs related to invalid data shapes as the codebase grows.

---
+++ src/interfaces/SkillsInterface.tsx
@@ -0,0 +1,13 @@
+// types.js
+
+// Define the types for your state
+export interface Content  {
+    category: string;
+    IT: string[];
+  };
+  
+export interface Posts {
+    title: string;
+    content: string;
+};
+  
