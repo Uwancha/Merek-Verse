1c21ed4e81dda6bb764b648a7231db3737939805
Uwancha <sito2alemu@gmail.com>
2023-11-07 21:13:34 +0300

    refactor: Normalize skills payload
    This refactors the getSkills thunk to normalize the skills payload before dispatching. It transforms the raw snapshot into an array of skill objects for more consistent handling downstream.

--- src/actions/skillsAction.js
+++ src/actions/skillsAction.js
@@ -1,11 +1,23 @@
-import { db } from '../firebase';
+import { db } from "../firestore";
+import { collection, getDocs } from "@firebase/firestore";
 
 export const getSkills = () => async dispatch => {
     const skillsRef = collection(db, 'skills');
     const skillsSnapshot = await getDocs(skillsRef);
 
+    const skills = [];
+
+    skillsSnapshot.forEach((doc) => {
+      skills.push({
+        name: doc.name, 
+        ...doc.data()
+      });
+    });
+
+    console.log(skills);
+
     dispatch({
         type: 'GET_SKILLS',
-        payload: skillsSnapshot
+        payload: skills
     } );
 }
