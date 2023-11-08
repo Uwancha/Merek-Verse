93ecfc0b9052c448382edc86feb8808b9d241a2e
Uwancha <sito2alemu@gmail.com>
2023-11-08 21:23:00 +0300

    refactor: Update the SkillsAction to fetch each Skill's details

--- src/actions/skillsAction.js
+++ src/actions/skillsAction.js
@@ -19,3 +19,26 @@ export const getSkills = () => async dispatch => {
         payload: skills
     } );
 }
+
+export const getSkillDetails = (category, skillId) => async (dispatch) => {
+  const categoryDocRef = doc(db, 'skills', category);
+  try {
+    const categoryDocSnap = await getDocs(categoryDocRef);
+    if (categoryDocSnap.exists()) {
+      const skillsData = categoryDocSnap.data().skills;
+      const skillDetails = skillsData.find(skill => skill.id === skillId);
+      if (skillDetails) {
+        dispatch({ type: 'GET_SKILL_DETAILS', payload: skillDetails });
+      } else {
+        // Handle if skill not found
+        console.log("Skill not found")
+      }
+    } else {
+      // Handle if category not found
+      console.log("Category not found")
+    }
+  } catch (error) {
+    // Handle error
+    console.error('Error getting skill details:', error);
+  }
+};
