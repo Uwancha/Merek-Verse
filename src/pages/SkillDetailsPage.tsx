201440127d54ed162c1a64146f94a28793ad93a6
Uwancha <sito2alemu@gmail.com>
2023-11-09 20:43:38 +0300

    chore: Display fetched skill details in SkillDetailsPage

--- src/pages/SkillDetailsPage.tsx
+++ src/pages/SkillDetailsPage.tsx
@@ -5,20 +5,40 @@ import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import { getSkillDetails } from '../actions/skillsAction';
 
-const SkillDetailsPage = ({ match, skillDetails, getSkillDetails }) => {
+const SkillDetailsPage = ({ skillDetails, getSkillDetails }) => {
   const { category, skillId } = useParams();
-  console.log(category)
 
   useEffect(() => {
-    getSkillDetails(category, skillId); // Dispatch action to get individual skill details
+    getSkillDetails(category, skillId);
   }, [category, skillId, getSkillDetails]);
 
   return (
     <div>
       {skillDetails && (
         <>
-          <h2>{skillDetails.name}</h2>
-          {/* Display other details of the skill using skillDetails */}
+          <h2>{skillDetails.description}</h2>
+          <h4>{skillDetails.name}</h4>
+          <p>{skillDetails.description}</p>
+          <h2>Industries</h2>
+          <p>{skillDetails.industries}</p>
+          <h2>Roles</h2>
+          <p>{skillDetails.roles}</p>
+          <h2>Career Outlook</h2>
+          <p>{skillDetails.careerOutlook}</p>
+          <h2>Learning Outcomes</h2>
+          <p>{skillDetails.learningOutcomes}</p>
+          <h2>prerequisites</h2>
+          <p>{skillDetails.prerequisites}</p>
+          <h2>Difficulty</h2>
+          <p>{skillDetails.difficulty}</p>
+          <h2>Resources</h2>
+          <p>{skillDetails.resources && skillDetails.resources.forEach(resource => {
+             <div>
+             <p>{resource.title}</p>
+             <p>{resource.type}</p>
+            </div>
+          }) }
+          </p>
         </>
       )}
     </div>
