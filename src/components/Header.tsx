6f4aff3462f92a56c1b4b06deb293d47a879ab51
Uwancha <sito2alemu@gmail.com>
2023-11-10 22:17:12 +0300

    design: Improve the design of the Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/header.css"
 import { Link } from "react-router-dom";
+
+
 const Nav: React.FC = () => {
 
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.scrollY;
+      setScrollPosition(position);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const headerStyle = {
+    backgroundColor: scrollPosition > 50 ? '#fff' : 'transparent', 
+    transition: 'background-color .3s ease',
+    position: 'fixed',
+    width: '100%',
+    top: 0,
+
+    display: 'flex',
+    padding: "0 4rem",
+    borderBottom:scrollPosition > 50 ? 'solid 1px rgba(119, 136, 153, 0.564)' : 'none',
+
+  }
+
+
     return (
-        <div className="header">
-            <h3>Merek Verse</h3>
+        <div style={headerStyle}>
+            <h3 className="header-h3">Merek Verse</h3>
             <nav className="nav">
-                <Link to="/" >Home</Link>
-                <Link to="/skills">Skills</Link>
-                <Link to="/blog">Blog</Link> 
-                <Link to="/about">About</Link>  
+                <Link to="/" className="header-a">Home</Link>
+                <Link to="/skills" className="header-a">Skills</Link>
+                <Link to="/blog" className="header-a">Blog</Link> 
+                <Link to="/about" className="header-a">About</Link>  
             </nav>
         </div>
     )
