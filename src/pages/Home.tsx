101fcf33cbb019d23ae45d9e477da789e8227116
Uwancha <sito2alemu@gmail.com>
2023-11-10 22:19:31 +0300

    design: Improve the design of the Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,59 +1,111 @@
 import React from "react";
 import "../styles/home.css"
 
+import sweImage from "../assets/software-developer-6521720_1280.jpg";
+import dataScience from "../assets/big-data-analytic-7050938_1280.jpg";
+import cloudComputing from "../assets/cloud-computing-2001090_1280.jpg";
+import uxuiIcon from "../assets/ux-787980_1280.jpg";
+import videoEditing from "../assets/video-editing-services-6373804_1280.jpg"
+import digitalMarketing from "../assets/digital-4368784_1280.jpg";
+
 const Home: React.FC = () => {
     
     return (
         <div className="home">
             <article className="hero">
-                <h2>Welcome!</h2>
-                <h3>
-                 A place where you find best curated resources for your first or next carrier!
-                </h3>
+                <h1>
+                 Home of curated resources for your first or next carrier!
+                </h1>
             </article>
 
-            <article className="article">
-                <h3>About us</h3>
+            <article className="article-about">
+                <h2>We're Here For You</h2>
                 <p>
-                    This site provides the best curated resources for in-demand skills like web development, design, marketing and more.
+                    We make the process of learning in-demand skills easy, providing the best curated resources and tips so that you don't have to waste time searching for resources!
                     Start your learning journey today!
                 </p>
-                <button className="button">Explore Skills</button>
+               
             </article>
 
-            <article className="article">
+            <article className="article-talents">
                 <h3>Most Sought-After Talents</h3>
                 
-                <section className="skill-card" >
-                    <h4>Software Engineering</h4>
-                    <p>A brief introduction about the skill...</p>
-                    <a href="">Link to the skill's detail page</a>
-                </section>
-                <section className="skill-card" >
-                    <h4>Data Science</h4>
-                    <p>A brief introduction about the skill...</p>
-                    <a href="">Link to the skill's detail page</a>
-                </section>
-                <section className="skill-card" >
-                    <h4>Cloud Computing</h4>
-                    <p>A brief introduction about the skill...</p>
-                    <a href="">Link to the skill's detail page</a>
-                </section>
-                <section className="skill-card" >
-                    <h4>UI/UX Design</h4>
-                    <p>A brief introduction about the skill...</p>
-                    <a href="">Link to the skill's detail page</a>
-                </section>
-                <section className="skill-card" >
-                    <h4>Video Editing</h4>
-                    <p>A brief introduction about the skill...</p>
-                    <a href="">Link to the skill's detail page</a>
-                </section>
-                <section className="skill-card" >
-                    <h4>Digital Marketing</h4>
-                    <p>A brief introduction about the skill...</p>
-                    <a href="">Link to the skill's detail page</a>
-                </section>
+                <div className="talents">
+                    <section className="skill-card" >
+                        <img src={sweImage} className="skill-image" alt="" />
+                        <h4>Software Engineering</h4>
+                        <p>A brief introduction about the skill...</p>
+                        <a href="">Learn More</a>
+                    </section>
+                    <section className="skill-card" >
+                        <img src={dataScience} className="skill-image" alt="" />
+                        <h4>Data Science</h4>
+                        <p>A brief introduction about the skill...</p>
+                        <a href="">Learn More</a>
+                    </section>
+                    <section className="skill-card" >
+                        <img src={cloudComputing} className="skill-image" alt="" />
+                        <h4>Cloud Computing</h4>
+                        <p>A brief introduction about the skill...</p>
+                        <a href="">Learn More</a>
+                    </section>
+                    <section className="skill-card" >
+                        <img src={uxuiIcon} className="skill-image" alt="" />
+                        <h4>UI/UX Design</h4>
+                        <p>A brief introduction about the skill...</p>
+                        <a href="">Learn More</a>
+                    </section>
+                    <section className="skill-card" >
+                        <img src={videoEditing} className="skill-image" alt="" />
+                        <h4>Video Editing</h4>
+                        <p>A brief introduction about the skill...</p>
+                        <a href="">Learn More</a>
+                    </section>
+                    <section className="skill-card" >
+                        <img src={digitalMarketing} className="skill-image" alt="" />
+                        <h4>Digital Marketing</h4>
+                        <p>A brief introduction about the skill...</p>
+                        <a href="">Learn More</a>
+                    </section>
+                </div>
+
+            </article>
+
+            <article className="article-blog">
+                <h3>Some of the Best Tips for Your Journey</h3>
+                
+                <div className="featured-blogs">
+                    <section className="blog-card" >
+                        <h4>How to escape the rabbit hole</h4>
+                        <p>A brief introduction about the topic...</p>
+                        <a href="">Read the Article</a>
+                    </section>
+                    <section className="blog-card" >
+                        <h4>What is a tutorial hell</h4>
+                        <p>A brief introduction about the topic...</p>
+                        <a href="">Read the Article</a>
+                    </section>
+                    <section className="blog-card" >
+                        <h4>Which skill Should I pick?</h4>
+                        <p>A brief introduction about the topic...</p>
+                        <a href="">Read the Article</a>
+                    </section>
+                    <section className="blog-card" >
+                        <h4>How to use AI as an aspiring Developer</h4>
+                        <p>A brief introduction about the topic...</p>
+                        <a href="">Read the Article</a>
+                    </section>
+                    <section className="blog-card" >
+                        <h4>Will AI replace programmers?</h4>
+                        <p>A brief introduction about the topic...</p>
+                        <a href="">Read the Article</a>
+                    </section>
+                    <section className="blog-card" >
+                        <h4>5 best Project ideas for front-end developer</h4>
+                        <p>A brief introduction about the topic...</p>
+                        <a href="">Read the Article</a>
+                    </section>
+                </div>
 
             </article>
         </div>
