a73139c7aaf0b22bc56277d359f8e3dfa63448cc
Uwancha <sito2alemu@gmail.com>
2023-11-11 09:31:27 +0300

    ui: Add images for featured blog posts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "../styles/home.css"
 
-import sweImage from "../assets/software-developer-6521720_1280.jpg";
-import dataScience from "../assets/big-data-analytic-7050938_1280.jpg";
-import cloudComputing from "../assets/cloud-computing-2001090_1280.jpg";
-import uxuiIcon from "../assets/ux-787980_1280.jpg";
-import videoEditing from "../assets/video-editing-services-6373804_1280.jpg"
-import digitalMarketing from "../assets/digital-4368784_1280.jpg";
+import rabbitHoleImage from "../assets/man-1204925_640.jpg";
+import tutorailHellImage from "../assets/man-7412527_640.png"
+import willAIImage from "../assets/artificial-intelligence-3382507_640.jpg";
+import aiImage from "../assets/artificial-intelligence-6767502_640.jpg";
+import pickSkillsImage from "../assets/cartoon-3082809_640.png";
+import projectIdeasImage from "../assets/website-3374825_640.jpg"
+
 
 const Home: React.FC = () => {
     
@@ -32,37 +33,32 @@ const Home: React.FC = () => {
                 
                 <div className="talents">
                     <section className="skill-card" >
-                        <img src={sweImage} className="skill-image" alt="" />
+                        
                         <h4>Software Engineering</h4>
                         <p>A brief introduction about the skill...</p>
                         <a href="">Learn More</a>
                     </section>
                     <section className="skill-card" >
-                        <img src={dataScience} className="skill-image" alt="" />
                         <h4>Data Science</h4>
                         <p>A brief introduction about the skill...</p>
                         <a href="">Learn More</a>
                     </section>
                     <section className="skill-card" >
-                        <img src={cloudComputing} className="skill-image" alt="" />
                         <h4>Cloud Computing</h4>
                         <p>A brief introduction about the skill...</p>
                         <a href="">Learn More</a>
                     </section>
                     <section className="skill-card" >
-                        <img src={uxuiIcon} className="skill-image" alt="" />
                         <h4>UI/UX Design</h4>
                         <p>A brief introduction about the skill...</p>
                         <a href="">Learn More</a>
                     </section>
                     <section className="skill-card" >
-                        <img src={videoEditing} className="skill-image" alt="" />
                         <h4>Video Editing</h4>
                         <p>A brief introduction about the skill...</p>
                         <a href="">Learn More</a>
                     </section>
                     <section className="skill-card" >
-                        <img src={digitalMarketing} className="skill-image" alt="" />
                         <h4>Digital Marketing</h4>
                         <p>A brief introduction about the skill...</p>
                         <a href="">Learn More</a>
@@ -76,32 +72,38 @@ const Home: React.FC = () => {
                 
                 <div className="featured-blogs">
                     <section className="blog-card" >
-                        <h4>How to escape the rabbit hole</h4>
+                        <h4>What is a rabbit hole and how to escape it</h4>
+                        <img src={rabbitHoleImage} className="blog-image" alt="" />
                         <p>A brief introduction about the topic...</p>
                         <a href="">Read the Article</a>
                     </section>
                     <section className="blog-card" >
                         <h4>What is a tutorial hell</h4>
+                        <img src={tutorailHellImage} className="blog-image" alt="" />
                         <p>A brief introduction about the topic...</p>
                         <a href="">Read the Article</a>
                     </section>
                     <section className="blog-card" >
                         <h4>Which skill Should I pick?</h4>
+                        <img src={pickSkillsImage} className="blog-image" alt="" />
                         <p>A brief introduction about the topic...</p>
                         <a href="">Read the Article</a>
                     </section>
                     <section className="blog-card" >
                         <h4>How to use AI as an aspiring Developer</h4>
+                        <img src={aiImage} className="blog-image" alt="" />
                         <p>A brief introduction about the topic...</p>
                         <a href="">Read the Article</a>
                     </section>
                     <section className="blog-card" >
                         <h4>Will AI replace programmers?</h4>
+                        <img src={willAIImage} className="blog-image" alt="" />
                         <p>A brief introduction about the topic...</p>
                         <a href="">Read the Article</a>
                     </section>
                     <section className="blog-card" >
                         <h4>5 best Project ideas for front-end developer</h4>
+                        <img src={projectIdeasImage} className="blog-image" alt="" />
                         <p>A brief introduction about the topic...</p>
                         <a href="">Read the Article</a>
                     </section>
