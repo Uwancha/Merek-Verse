aa95a1cc13781f1444772df3b017fbda6cdba723
Uwancha <sito2alemu@gmail.com>
2023-11-11 10:57:56 +0300

    feat: Add Loading component for a better user experience

---
+++ src/components/Loading.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/loading.css"
+
+export const Loading = () => {
+    return (
+        <div className="loading">
+            <button className="circular-btn"></button>
+            <button className="circular-btn"></button>
+            <button className="circular-btn"></button>
+        </div>
+    )
+}
+
