80ce794b888c99f6add2ff49a1fda2553b8c88a0
Uwancha <sito2alemu@gmail.com>
2023-11-11 22:52:24 +0300

    refactor: Refine the content of the Blog page

--- src/blog/Blog.tsx
+++ src/blog/Blog.tsx
@@ -1,61 +1,104 @@
 import React from "react";
-
-import rabbitHoleImage from "../assets/man-1204925_640.jpg";
-import tutorailHellImage from "../assets/man-7412527_640.png"
-import willAIImage from "../assets/artificial-intelligence-3382507_640.jpg";
-import aiImage from "../assets/artificial-intelligence-6767502_640.jpg";
-import pickSkillsImage from "../assets/cartoon-3082809_640.png";
-import projectIdeasImage from "../assets/website-3374825_640.jpg"
 import Nav from "../components/Header";
+
+import "../styles/blog.css";
+
+import authorImage from "../assets/myPhoto.jpeg"
 
 
 export const Blog: React.FC = () => {
     return (
-        <>
+        <div style={{backgroundColor: "whitesmoke"}}>
         <Nav />
         <article className="blog">
-            <h3>Some of the Best Tips for Your Journey</h3>
-                
-            <div className="featured-blogs">
-                <section className="blog-card" >
-                    <h4>What is a rabbit hole and how to escape it</h4>
-                    <img src={rabbitHoleImage} className="blog-image" alt="" />
-                    <p>A brief introduction about the topic...</p>
-                    <a href="">Read the Article</a>
+            <div className="posts">
+                <section className="blogCard" >
+                    <div className="blogCard-title" >
+                        <img src={authorImage} className="authorImage" alt="Author's image" />
+                        <div className="author-date" >
+                            <p>Sitota Alemu</p>
+                            <p>Nov, 2023</p>
+                        </div>
+                    </div>
+                    <h2>What is a rabbit hole and how to escape it</h2>
+                    <div className="cta" >
+                        <p>5 min read</p>
+                        <a href="">Continue Reading</a>
+                    </div>
                 </section>
-                <section className="blog-card" >
-                    <h4>What is a tutorial hell</h4>
-                    <img src={tutorailHellImage} className="blog-image" alt="" />
-                    <p>A brief introduction about the topic...</p>
-                    <a href="">Read the Article</a>
+                <section className="blogCard" >
+                    <div className="blogCard-title" >
+                        <img src={authorImage} className="authorImage" alt="Author's image" />
+                        <div className="author-date" >
+                            <p>Sitota Alemu</p>
+                            <p>Nov, 2023</p>
+                        </div>
+                    </div>
+                    <h2>What is a tutorial hell</h2>
+                    <div className="cta" >
+                        <p>5 min read</p>
+                        <a href="">Continue Reading</a>
+                    </div>
                 </section>
-                <section className="blog-card" >
-                    <h4>Which skill Should I pick?</h4>
-                    <img src={pickSkillsImage} className="blog-image" alt="" />
-                    <p>A brief introduction about the topic...</p>
-                    <a href="">Read the Article</a>
+                <section className="blogCard" >
+                    <div className="blogCard-title" >
+                        <img src={authorImage} className="authorImage" alt="Author's image" />
+                        <div className="author-date" >
+                            <p>Sitota Alemu</p>
+                            <p>Nov, 2023</p>
+                        </div>
+                    </div>
+                    <h2>Which skill Should I pick?</h2>
+                    <div className="cta" >
+                        <p>5 min read</p>
+                        <a href="">Continue Reading</a>
+                    </div>
                 </section>
-                <section className="blog-card" >
-                    <h4>How to use AI as an aspiring Developer</h4>
-                    <img src={aiImage} className="blog-image" alt="" />
-                    <p>A brief introduction about the topic...</p>
-                    <a href="">Read the Article</a>
+                <section className="blogCard" >
+                    <div className="blogCard-title" >
+                        <img src={authorImage} className="authorImage" alt="Author's image" />
+                        <div className="author-date" >
+                            <p>Sitota Alemu</p>
+                            <p>Nov, 2023</p>
+                        </div>
+                    </div>
+                    <h2>How to use AI as an aspiring Developer</h2>
+                    <div className="cta" >
+                        <p>5 min read</p>
+                        <a href="">Continue Reading</a>
+                    </div>
                 </section>
-                <section className="blog-card" >
-                    <h4>Will AI replace programmers?</h4>
-                    <img src={willAIImage} className="blog-image" alt="" />
-                    <p>A brief introduction about the topic...</p>
-                    <a href="">Read the Article</a>
+                <section className="blogCard" >
+                    <div className="blogCard-title" >
+                        <img src={authorImage} className="authorImage" alt="Author's image" />
+                        <div className="author-date" >
+                            <p>Sitota Alemu</p>
+                            <p>Nov, 2023</p>
+                        </div>
+                    </div>
+                    <h2>Will AI replace programmers?</h2>
+                    <div className="cta" >
+                        <p>5 min read</p>
+                        <a href="">Continue Reading</a>
+                    </div>
                 </section>
-                <section className="blog-card" >
-                    <h4>5 best Project ideas for front-end developer</h4>
-                    <img src={projectIdeasImage} className="blog-image" alt="" />
-                    <p>A brief introduction about the topic...</p>
-                    <a href="">Read the Article</a>
+                <section className="blogCard" >
+                    <div className="blogCard-title" >
+                        <img src={authorImage} className="authorImage" alt="Author's image" />
+                        <div className="author-date" >
+                            <p>Sitota Alemu</p>
+                            <p>Nov, 2023</p>
+                        </div>
+                    </div>
+                    <h2>5 best Project ideas for front-end developer</h2>
+                    <div className="cta" >
+                        <p>5 min read</p>
+                        <a href="">Continue Reading</a>
+                    </div>
                 </section>
                 </div>
 
         </article>
-        </>
+        </div>
     )
 }
